585ba753920e3ec2d8a623be0786a5f36ad978ca
nada <maowod.nada69@gmail.com>
2022-08-29 17:01:00 +0200

    [Add] order fragment

--- app/src/main/java/com/iei/dotshop/ui/main/MainActivity.kt
+++ app/src/main/java/com/iei/dotshop/ui/main/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.iei.dotshop.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.iei.dotshop.R
+import com.iei.dotshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initNav()
+    }
+
+    private fun initNav() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
