1372c40285b5866e091ab25010e844c17260094e
Islam Shadeed <37776598+isllamm@users.noreply.github.com>
2022-08-30 15:40:51 +0200

    DotShop

--- app/src/main/java/com/iei/dotshop/ui/rate/ReviewsFragment.kt
+++ app/src/main/java/com/iei/dotshop/ui/rate/ReviewsFragment.kt
@@ -6,17 +6,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.iei.dotshop.R
+import com.iei.dotshop.databinding.FragmentRateOrderBinding
+import com.iei.dotshop.databinding.FragmentReviewsBinding
+import com.iei.dotshop.ui.main.MainActivity
 
 
 class ReviewsFragment : Fragment() {
+
+    private lateinit var binding: FragmentReviewsBinding
+    private lateinit var parent: MainActivity
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reviews, container, false)
+    ): View {
+        binding = FragmentReviewsBinding.inflate(inflater)
+        parent = requireActivity() as MainActivity
+
+        setupUI()
+        onClick()
+        return binding.root
+    }
+
+    private fun onClick() {
+        binding.btnContinue1.setOnClickListener {
+            parent.navController.navigate(R.id.action_reviewsFragment_to_rateOrderFragment)
+        }
+    }
+
+    private fun setupUI() {
+        parent.showToolbar(false)
+        parent.showBottomNav(false)
     }
 
 
